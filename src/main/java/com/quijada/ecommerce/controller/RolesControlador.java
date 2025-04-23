5456e3756fb53e24280012fd52115ed190762368
José Ian Perez Gomez <59273923+Wegenner@users.noreply.github.com>
2025-04-23 06:40:40 -0600

    Creamos controladores para cada una de las entidades

---
+++ src/main/java/com/quijada/ecommerce/controller/RolesControlador.java
@@ -0,0 +1,9 @@
+package com.quijada.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/roles/")
+public class RolesControlador {
+}
