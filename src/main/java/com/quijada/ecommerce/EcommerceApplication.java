b5c0ad2dbe619a4c5b88fe36d4b0d5b7fe5d51ac
Antonio <antonio.mel9814@gmail.com>
2025-04-22 22:43:26 -0600

    adaptacion .properties

--- src/main/java/com/quijada/ecommerce/EcommerceApplication.java
+++ src/main/java/com/quijada/ecommerce/EcommerceApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommerceApplication {
 
-	static{
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-	}
+//	static{
+//		Dotenv dotenv = Dotenv.load();
+//		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
